88ab333e190afbd30cafa1877291d7abb7a8cc51
Amen Alahassa <am.konrad21@gmail.com>
2021-07-13 21:10:11 +0100

    Integrate template and make Login UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './views/authentication/login/login.component';
+import {DashboardComponent} from './views/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    data : {title : 'login'},
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    data : {title : 'login'},
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    data : {title : 'login'},
+    component: DashboardComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SharedModule} from './shared/shared.module';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { LoginComponent } from './views/authentication/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SharedModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
