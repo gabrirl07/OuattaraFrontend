5e323e66c66d14df627c9ba78d6c92dbd24d874b
Amen Alahassa <am.konrad21@gmail.com>
2021-07-20 20:19:20 +0100

    Change visa details title

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   encryptPassword: 'ouattara-encrypt',
-  BASE_URL: 'https://dubaievisa.bssi.ca:8180/api',
+  BASE_URL: 'http://dubaievisa.bssi.ca:8180/api',
   client_id: "1_qd10xlcxirk4wcgsskg8c8gogw4so8okgw4kcs8kc44go0gos",
   client_secret: "268nzzcgm2n4kk8sscg4gskk0wws0g0gs4s0sgkogss0sg4gc8",
   grant_type: "password",
