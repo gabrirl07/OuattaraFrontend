14b106a3f1e475f5268b2071c13b0762cf1093e6
ALAHASSA Konrad <am.konrad21@gmail.com>
2022-11-19 15:24:27 +0100

    wip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,11 @@ export const environment = {
   production: true,
   encryptPassword: 'ouattara-encrypt',
   BASE_URL: 'https://dubaievisa.bssi.ca',
-  BASE_AUTH_API_URL: `https://evisa.bssi.ca/`,
+  BASE_AUTH_API_URL: `https://evisa.bssi.ca`,
   BASE_API_URL: `https://evisa.bssi.ca/api`,
   client_id : "1_25rl2i08ueiskwckcg08s8ock8o08ww08occcg4s8k0soc4sco",
   client_secret : "2him7ohltiyow0cwswkw8g4ckgg4sgwwo8sw844co48g8owggk",
   grant_type : "password",
+//      "username" : "test@mail.com",
+//     "password" : "testOuatarra2021"
 };
