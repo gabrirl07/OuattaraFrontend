6af6c3357c384bd630cdb334f8f958a6d1a4e766
ALAHASSA Konrad <am.konrad21@gmail.com>
2022-11-19 12:05:28 +0100

    UPdate secret key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   encryptPassword: 'ouattara-encrypt',
   BASE_URL: 'https://dubaievisa.bssi.ca',
   BASE_API_URL: `https://evisa.bssi.ca`,
-  client_id : "1_ktd97my6hxsc8kswo00cscwsw80ck88g0gsg4w048osgwo4go",
-  client_secret : "5y0q8x0vls4kscwk48ckgk8gc80s4wo0ok0o4g48cc4wgg0osw",
+  client_id : "1_25rl2i08ueiskwckcg08s8ock8o08ww08occcg4s8k0soc4sco",
+  client_secret : "2him7ohltiyow0cwswkw8g4ckgg4sgwwo8sw844co48g8owggk",
   grant_type : "password",
 };
